b0d1d58f983081307afe063ffb1bff32c2f4803f
hugo.drevet@hotmail.fr <Riveraine1*>
2022-10-12 16:52:23 +0200

    [REPO] organize dir
    [PHP / HTML]
    added a placeholder logo

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceuil</title>
+
 </head>
 
 
 
 <body>
+    <img src=".\Asset\General\logo.jpg" alt="Logo De La Société" />
+
 
 </body>
 
