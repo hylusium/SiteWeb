a13da9d93b7fded7695b0dc0e6e0955dfcedef39
hugo.drevet@hotmail.fr <Riveraine1*>
2022-10-19 14:37:41 +0200

    index css and index.php

--- index.php
+++ index.php
@@ -12,11 +12,17 @@
 
 
 <body>
+
+    <div>
+        <h1 id="ACME">ACME</h1>
+    </div>
+
     <div>
 
         <img src="Asset\General\logo.jpg" alt="Logo De La Société" id="logo" />
 
     </div>
+
 
 
 </body>
